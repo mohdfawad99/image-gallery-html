129ca2d0ce3fe65019bfe0975ca2761c5f5fe302
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-10-26 17:25:55 +0530

    dark theme added

--- script.js
+++ script.js
@@ -33,3 +33,22 @@ popup = {
   }
   
 popup.init()
+
+const chk = document.getElementById('chk');
+
+chk.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
